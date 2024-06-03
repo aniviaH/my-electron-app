8576e847e8513faa03ebbad734874fb66652692c
刘欢 <huan.liu@casstime.com>
2024-06-03 10:50:48 +0800

    feat: Tutorial - loading a web page into a BrowserWindow [https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app]

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello from Electron 👋')
+// console.log('Hello from Electron 👋')
+
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+// app.on('ready', () => {
+app.whenReady().then(() => {
+  createWindow()
+})
